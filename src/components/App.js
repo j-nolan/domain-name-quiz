9d4a81680531cb0d888c38df41c017c5bb568ca7
James Nolan <j.nolan@otherwise.ch>
2016-11-14 18:57:05 +0100

    initial commit. App is not ready yet

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from './Header';
+import Button from './Button';
+
+const App = () => (
+  <div className="app-wrapper">
+    <Header />
+    <Button>Take the quiz!</Button>
+  </div>
+);
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+  <div>
+    <h1 className="app-title">domain name quiz</h1>
+    <p className="tag-line">How well do you know domain names?</p>
+  </div>
+);
+
+export default Header;
